51a584d515a7b38cc421a2e1ba8247beb344b74f
Ahmed-Ashraf7 <116293693+Ahmed-Ashraf7@users.noreply.github.com>
2022-12-12 21:45:15 +0200

    Add files via upload

---
+++ myproject/MyProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project.myproject;
+
+/**
+ *
+ * @author a
+ */
+public class MyProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
